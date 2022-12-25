c610788816dfa3e62f4a76ad5216ee759f0f406a
MKay117 <kmkmonu1996@gmail.com>
2022-12-25 16:44:59 +0530

    Login page

--- blogs/src/App.js
+++ blogs/src/App.js
@@ -1,21 +1,27 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import UserContext from './components/UserContext';
+import ProtectedRoute from './components/ProtectedRoute';
+import { useState } from 'react';
 
 function App() {
+
+  const [isLogged, setIsLogged] = useState(false);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <UserContext.Provider value={ {username, setUsername, password, setPassword, isLogged, setIsLogged} }>
+        <Router>
+          <Routes>
+            <Route exact path="/login" element={<Login />} />
+            <Route exact path="/" element={<ProtectedRoute>{<Home />} </ProtectedRoute>}/>
+          </Routes>
+        </Router>
+      </UserContext.Provider>
     </div>
   );
 }
